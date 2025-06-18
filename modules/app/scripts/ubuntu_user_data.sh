88da59612eb16de67d4d42b57d4491f58c6f4c3b
laulv <levanlau.net@gmail.com>
2025-06-18 10:39:19 +0700

    update

--- modules/app/scripts/ubuntu_user_data.sh
+++ modules/app/scripts/ubuntu_user_data.sh
@@ -11,4 +11,11 @@ systemctl start nginx
 apt update -y
 apt install -y certbot python3-certbot-nginx
 # 
+apt update -y
+apt install -y unzip curl
+cd /tmp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install
+# 
 echo "Setup done" > /var/log/userdata.log
